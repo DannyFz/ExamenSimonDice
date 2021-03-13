ed1dd009c5b5ef63ac983bd3bc9582204abb1cd2
DannyFz <71345818+DannyFz@users.noreply.github.com>
2021-03-12 22:23:26 -0600

    Examen

---
+++ SimonDice/SimonDice/Extensiones/UIButtonExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIButtonExtension.swift
+//  SimonDice
+//
+//  Created by mac23 on 05/03/21.
+//
+
+import UIKit
+
+extension UIButton {
+
+    func round(){
+        layer.cornerRadius = bounds.height / 2
+    }
+}
---
+++ SimonDice/SimonDice/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SimonDice
+//
+//  Created by mac23 on 05/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btn1: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        btn1.round()
+
+        
+        
+        
+        
+        
+        
+    }
+
+
+}
+
